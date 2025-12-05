45f5a900784bed7e515bd2d29046ed51783221c0
Aleksei <gsgzbjssrgdgnjevecuh@gmail.com>
2025-12-05 22:37:38 +0300

    PORT

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = Number(process.env.PORT) || 3001;
 
 app.use(cors());
 app.use(express.json());
@@ -185,5 +185,5 @@ setInterval(() => {
 }, 10_000);
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
